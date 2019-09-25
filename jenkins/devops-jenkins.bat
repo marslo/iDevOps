8900a67a8bb58cd54b9429d36f172e0da9dca1cc
marslo <marslo@marvell.com>
2019-09-25 13:24:10 +0800

    updaste

--- jenkins/devops-jenkins.bat
+++ jenkins/devops-jenkins.bat
@@ -1,3 +1,2 @@
 sc.exe create "devops-jenkins" start= auto binPath= "J:\devops\devops-jenkins-slave.exe" DisplayName= "devops-jenkins"
-
 sc.exe create "devops-jenkins" start= auto binPath= "J:\devops\devops-jenkins-slave.exe" DisplayName= "devops-jenkins"
