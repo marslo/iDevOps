064a982e2c751308b805948e805454f2a2b08101
marslo <marslo.jiao@gmail.com>
2021-01-18 23:32:49 +0800

    add jenkins script for build history

---
+++ jenkins/scripts/getBuildsHistory.groovy
@@ -0,0 +1,41 @@
+// get single build
+def buildName = Jenkins.instance.getItemByFullName('<group>/<name>')
+println "All builds: "
+buildName.getBuilds().each{
+  println """
+              id : ${it.getId()}
+    build number : ${it.getNumber()}
+     start since : ${it.getTimestampString()}
+     xs:dateTime : ${it.getTimestampString2()}
+        duration : ${it.getDurationString()}
+  """
+}
+
+
+
+// get single build within 24 hours
+def numberOfHoursBack = 1*24
+def job = Jenkins.instance.getItemByFullName('<group>/<jobName>')
+def history = job.getBuilds().byTimestamp(System.currentTimeMillis()-numberOfHoursBack*60*60*1000, System.currentTimeMillis())
+println """
+  history.size(): ${history.size()}
+  history: ${history}
+"""
+
+
+
+// get history in a group
+String jobPattern = '<group>/'
+def numberOfHoursBack = 1*24
+
+Jenkins.instance.getAllItems(Job.class).findAll { Job job ->
+  job.fullName.contains(jobPattern)
+}.collect { Job job ->
+  println "~~~> job.fullName: ${job.fullName}"
+  def history = job.getBuilds().byTimestamp(System.currentTimeMillis()-numberOfHoursBack*60*60*1000, System.currentTimeMillis())
+    println """
+      ${job.fullName}: ${history.size()}
+      history: ${history}
+    """
+}
+println "DONE"
