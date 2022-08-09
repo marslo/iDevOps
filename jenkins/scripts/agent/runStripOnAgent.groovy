0c94bd821a46cfc928624c72c69938200835377a
marslo <marslo.jiao@gmail.com>
2022-08-09 22:07:21 +0800

    update script approval

--- jenkins/scripts/agent/runStripOnAgent.groovy
+++ jenkins/scripts/agent/runStripOnAgent.groovy
@@ -3,19 +3,19 @@
 
 import hudson.util.RemotingDiagnostics
 import jenkins.model.Jenkins
- 
-String agent_name = 'your slave name'
+
+String agent_name = 'your agent name'
 //groovy script you want executed on an agent
 groovy_script = '''
 println System.getenv("PATH")
 println "uname -a".execute().text
 '''.trim()
- 
+
 String result
 Jenkins.instance.slaves.find { agent ->
-    agent.name == agent_name
+  agent.name == agent_name
 }.with { agent ->
-    result = RemotingDiagnostics.executeGroovy(groovy_script, agent.channel)
+  result = RemotingDiagnostics.executeGroovy( groovy_script, agent.channel )
 }
 println result
 
